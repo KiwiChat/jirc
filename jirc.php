ca9703098a5b26f5689c0ead386ae8a5ce75bf72
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2023-12-16 19:45:03 +0100

    Create jirc.php

---
+++ jirc.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: JIRC
+Plugin URI: https://github.com/kiwichat/jirc
+Description: Allows you to embed a KiwiIRC IRC client with a shortcode.
+Version: 1.0
+Author URI: https://HatchetShack.com
+License: GNU General Public License
+*/
+// https://hatchetshack.com/JIRC/?nick=Juggalo_49564&theme=sky#HatchetRadio 
+function jirc_embed_func($atts) {
+	extract(shortcode_atts(array(
+		'width'		=> '100%',
+		'height'	=> '550px',
+		'channel'	=> '#HatchetRadio ',
+		'theme'	    => 'osprey'
+	), $atts));
+		//User Nick
+	if(!is_user_logged_in()) {
+		$nick = __('Juggalo??', 'jirc');
+	} else {
+		global $current_user;
+		get_currentuserinfo();
+		$nick = $current_user->display_name;
+	}
+	return '<iframe style="width:'.$width.';height:'.$height.';border:0;" src="https://hatchetshack.com/JIRC/?nick='.$nick.'&theme='.$theme.''.$channel.'"></iframe>';
+}
+add_shortcode('jirc', 'jirc_embed_func');
+?>
